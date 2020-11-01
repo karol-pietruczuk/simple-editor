f26f41b0f09a5a090a231aedabc967f0dece6acb
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2020-11-02 00:49:58 +0100

    Design, HTML, SCSS, JS commit because of repo fault

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,45 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO 🚀');
+
+let result = '';
+//code from this place
+const entry = localStorage.getItem('entry');
+if (entry) {
+    console.log(`wartość entry: ${entry}`);
+    result = entry;
+}
+//to this place is because of indicating actual value of variable localStorage entry
+
+const entryInput = document.querySelector('.entry--js');
+const buttonRead = document.querySelector('.button__read--js');
+const buttonSave = document.querySelector('.button__save--js');
+
+
+buttonRead.addEventListener('click', (e) => {
+    entryInput.value = localStorage.getItem('entry');
+});
+
+buttonSave.addEventListener('click', () => {
+    localStorage.setItem('entry', entryInput.value)
+});
+
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(btn => {
+    btn.addEventListener('click', function(ev){
+     
+        let x = ev.clientX - ev.target.offsetLeft; //ev.clientX - x right button position + mouse movement from left extreme button position || ev.target.offsetLeft - x right button position
+        let y = ev.clientY - ev.target.offsetTop + scrollY; //scrollY - Y position of scroll bar
+        let ripples = document.createElement('span');
+        
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        this.appendChild(ripples);
+
+        setTimeout(() => {
+            ripples.remove()
+        },1000);
+    })
+})
